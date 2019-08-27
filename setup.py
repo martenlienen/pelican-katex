a0c38279301d9db52941fd4703b055cc580475a7
Marten Lienen <marten.lienen@gmail.com>
2019-08-27 15:52:10 +0200

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pelican-katex",
-    version="1.1.0",
+    version="1.2.0",
     author="Marten Lienen",
     author_email="marten.lienen@gmail.com",
     description="Server-side LaTeX typesetting for pelican",
