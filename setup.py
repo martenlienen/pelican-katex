1f7eeedbe7828368cc5e73db08a61309a2874f67
Marten Lienen <marten.lienen@gmail.com>
2020-02-06 23:24:49 +0100

    Mark release 1.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pelican-katex",
-    version="1.3.1",
+    version="1.4.0",
     author="Marten Lienen",
     author_email="marten.lienen@gmail.com",
     description="LaTeX pre-rendering for pelican",
